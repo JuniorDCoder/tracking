0e5c6ef3b2bb981e1ec6897857db575eafbbf007
JuniorDCoder <foryoungjuniorngu@gmail.com>
2025-03-07 19:56:36 +0100

    feat: Change images

--- db.php
+++ db.php
@@ -1,18 +1,17 @@
 <?php
 
 @define('DB_SERVER', 'localhost');
-@define('DB_USERNAME', 'root');
-@define('DB_PASSWORD', '');
-@define('DB_NAME', 'shipping');
+@define('DB_USERNAME', 'u820100908_upsglobalship');
+@define('DB_PASSWORD', 'upsglobalship@A1');
+@define('DB_NAME', 'upsglobalshipu820100908_upsglobalship');
 
- 
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
- 
+
 // Check connection
-if($link === false){
+if ($link === false) {
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
 
 ?>
-
